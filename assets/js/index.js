7527bedf2d3ee3e9a738e26608791b2247a532c7
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-03-31 21:09:47 +0300

    Add array example

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const userTel1 = '38042748374';
+const userTel2 = '38042748374';
+const userTel3 = '38042748374';
+
+const userTels = {
+  0: '38089347595',
+  1: '38042748374',
+  2: '38454548376',
+};
+
+userTels[3] = '38000348394';
+
+const arr = ['38089347595', '38042748374', '38454548376', '38000348394'];
+arr[4] = 'test';
+
+
+console.log(userTels);
+console.log(arr);
